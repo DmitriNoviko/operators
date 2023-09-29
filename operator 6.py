b1ad6c2cf0f07927224d11edb3bf331eb1707727
DmitriNoviko <143862586+DmitriNoviko@users.noreply.github.com>
2023-09-29 17:07:06 +0300

    Update operator 6.py

--- operator 6.py
+++ operator 6.py
@@ -2,11 +2,11 @@ import math
 
 x = float(input("Введите координату x: "))
 y = float(input("Введите координату y: "))
-radius = float(input("Введите радиус круга: "))
+r = float(input("Введите r круга: "))
 
-distance = math.sqrt(x ** 2 + y ** 2)
+d = math.sqrt(x ** 2 + y ** 2)
 
-if distance <= radius:
+if d <= r:
     print("Точка принадлежит кругу")
 else:
     print("Точка не принадлежит кругу")
